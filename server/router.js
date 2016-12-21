f9644a5396b708d61f4976d4204e50c5c9798237
saikatcts <Saikat.Bhattacharya5@cognizant.com>
2016-12-21 09:41:59 +0530

    DAL working

--- server/router.js
+++ server/router.js
@@ -1,18 +1,27 @@
 var requestHandler = require('../middleware/requestHandler');
 var bodyParser = require('body-parser');
+var multer = require("multer");
+var upload = multer({ dest: "./uploads" });
+var path = require('path');
+
 module.exports = function (app) {
     // parse application/x-www-form-urlencoded 
     app.use(bodyParser.urlencoded({ extended: false }))
 
     // parse application/json 
     app.use(bodyParser.json())
-    
+
     app.get('/', function (req, res) {
-        res.send('hello world')
+        res.sendFile(path.join(__dirname + '/index.html'));
     })
 
     app.post('/myname', function (req, res) {
         console.log(" Sending request..... ", req.body);
         requestHandler.postMyname(req.body.name, res);
     })
+
+    app.post("/fileupload", upload.single("filename"), function (req, res) {
+        console.log("***** ", req.files);
+        requestHandler.postFile(req, res);
+    });
 }
